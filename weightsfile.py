c807e07ad653a7eaa29a505afea8b74da819756a
tauseefnawaz75 <tauseefnawaz75@gmail.com>
2020-12-18 21:29:31 +0500

    added requirement.txt yolov3 and weights file

---
+++ weightsfile.py
@@ -0,0 +1,8 @@
+import gdown
+import os
+#download classes
+os.system("gdown --id 1-BuEDvOuYDcRNmh_35uMUjA8Cfe0THkE")
+#downlaod cfg file
+os.system("gdown --id 1-3-YwP2ink_i_Z5i6XXvHDb9ZTQAsAA5")
+#download weights
+os.system("gdown --id 1Bj3BwW5V6F3M0E_lkX-i_p6X3JtX6eJw")
